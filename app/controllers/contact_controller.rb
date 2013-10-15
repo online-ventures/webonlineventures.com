60979df2c9f3c4d5068140401bc8e1d489b3f073
Jacob Evan Shreve <jacob@shreve.ly>
2013-10-14 23:01:23 -0400

    Don't even bother sending the mail if it's garbage

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,7 +2,7 @@ class ContactController < ApplicationController
 	skip_before_action :verify_authenticity_token
 	
 	def send_message
-		ContactMailer.send_using(params).deliver
+		ContactMailer.send_using(params).deliver unless params['honeypot'].present?
 		respond_to do |format|
 			format.json { render json: { status: 'success', message: 'We\'ve recieved your message.<br/> Someone will get back to you soon.'.html_safe} }
 		end
